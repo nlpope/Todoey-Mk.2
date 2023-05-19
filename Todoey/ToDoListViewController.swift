133ace70b9b202c69fd86c016ca313b50b02355b
Noah Pope <npope@tutanota.com>
2023-05-19 06:54:19 -0400

    testing

--- Todoey/ToDoListViewController.swift
+++ Todoey/ToDoListViewController.swift
@@ -12,7 +12,7 @@ class ToDoListViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // testing commit
     }
 
 
