4b6de0ff4b099f88791370a5b903e13f967ce485
Noah Pope <npope@tutanota.com>
2023-05-23 09:44:17 -0400

    setting up delegate & dataSource methods

--- Todoey/ToDoListViewController.swift
+++ Todoey/ToDoListViewController.swift
@@ -8,7 +8,11 @@
 
 import UIKit
 
+//UITableViewController by default conforms to UITableViewDatasource & ..Delegate
+//..conforms = no UITab....Source...Delegate calls needed but must still be defined in ext
 class ToDoListViewController: UITableViewController {
+    
+    let itemArray = ["Find Mike", "Buy Eggos", "Destroy Demogorgon"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,5 +27,25 @@ class ToDoListViewController: UITableViewController {
     }
 
 
+}
+
+
+extension ToDoListViewController  {
+    //MARK: TableView DataSource Methods
+    //..conforms = no UITab....Source...Delegate calls needed but must still be defined in ext
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        cell.textLabel?.text = itemArray[indexPath.row]
+        return cell
+    }
+    
+    //MARK: TableView Delegate Methods
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(itemArray[indexPath.row])
+    }
 }
 
